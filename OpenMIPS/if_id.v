4cfb6a7ab54684a36d2e1cc3923850f047cbdaec
mfxin <1579865023@qq.com>
2022-11-21 21:38:16 +0800

    OpenMIPS

---
+++ OpenMIPS/if_id.v
@@ -0,0 +1,22 @@
+module if_id(
+  input     clk,
+  input     rst,
+  input [`InstAddrBus]  if_pc,
+  input [`InstBus]      if_inst,
+
+  output reg[`InstAddrBus]  id_pc,
+  output reg[`InstBus]      id_inst
+);
+
+always @(posedge clk)begin
+  if(rst == `RstEnable)begin
+    id_pc <= `ZeroWord;
+    id_inst <= `ZeroWord;
+  end
+  else begin
+    id_pc <= if_pc;
+    id_inst <= if_inst;
+  end
+end
+
+endmodule
