bc058805f25435c0aaa23c8c33f7f50e7fb685a4
mfxin <1579865023@qq.com>
2022-11-25 22:02:21 +0800

    OpenMIPS

--- OpenMIPS/inst_rom.v
+++ OpenMIPS/inst_rom.v
@@ -1,3 +1,4 @@
+`include "defines.v"
 module inst_rom(
   input ce,
   input [`InstAddrBus] addr,
--- OpenMIPS/openmips_min_sopc.v
+++ OpenMIPS/openmips_min_sopc.v
@@ -8,14 +8,14 @@ wire ce_i;
 wire[`InstBus] inst_o;
 
 inst_rom inst_rom0(
-  .clk    (clk),
-  .rst    (rst),
   .addr   (addr_i),
   .ce     (ce_i),
   .inst   (inst_o)
 );
 
 openmips  openmips0(
+  .clk    (clk),
+  .rst    (rst),
   .rom_ce_o   (ce_i),
   .rom_addr_o (addr_i),
   .rom_data_i (inst_o)
